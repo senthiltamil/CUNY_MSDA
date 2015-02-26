b967c46e800992961b09cb24090190c360421fc0
senthiltamil <senthiltamil@gmail.com>
2015-02-25 17:54:58 -0800

    Version 2
    Added few more commands that I learnt in office hours today

--- IS607Week5Assignment_LoadCSV.R
+++ IS607Week5Assignment_LoadCSV.R
@@ -9,7 +9,21 @@ names(flights)[names(flights)=="air_time"] <- "[Air Time in Minutes]"
 names(flights)[names(flights)=="seats"] <- "[Seating Capacity of Airplane]"
 
 #show sample rows
-flights[1:10,]
+flights[1:10,3:4]
+
 
 #show all rows
 flights
+
+summary(flights)
+
+#learnt in office hours
+class(flights)
+str(flights)
+head(flights)
+View(flights)  
+
+
+
+flightsubset <- flights[3:4]
+summary(flightsubset)
